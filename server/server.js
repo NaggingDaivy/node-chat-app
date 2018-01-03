0117cf175b4b1cb0a587ee94ea4d2191817a488e
Daivy Merlijs <daivymerlijs@MacBook-Pro-de-Daivy.local>
2018-01-03 11:43:21 +0100

    Add connect and disconnect even handlers

--- server/server.js
+++ server/server.js
@@ -1,16 +1,33 @@
-var express = require('express');
 const path = require('path');
-
-var app = express();
+const http = require('http');
+var express = require('express');
+const socketIO = require('socket.io');
 
 const port = process.env.PORT || 3000;
-
 const publicPath = path.join(__dirname,'../public');
+var app = express();
+// var server = http.createServer((req,res) => { // équivalent à app.listen
+
+// }) 
+var server = http.createServer(app);
+var io = socketIO(server); // socketIO dispo sur localhost:3000/socket.io/socket.io.js
 
 
 app.use(express.static(publicPath));
-app.listen(port, () => {
+// app.listen(port, () => {
+//     console.log(`Server started on port ${port}`);
+// })
+
+io.on('connection', (socket) => { // ce qui se passe qd 1 client se connecte au serveur
+    console.log('New user connected');
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+
+    });
+
+});
+
+server.listen(port, () => {
     console.log(`Server started on port ${port}`);
 })
-
-//eexpress static to fill public folder + appeler sur port 3000
